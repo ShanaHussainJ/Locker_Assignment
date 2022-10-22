e42e5ca49b94493ef5766294ab3a55f971b53924
shana <jshana0103@gmail.com>
2022-10-22 21:34:10 +0530

    for system

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ function enterpwd(){
     var text=document.getElementById("pwd").value
     if (text == 1234) {
         alert("Success!");
+        document.getElementById("pwd").value=""
       } else {
         alert("Incorrect Password");
+        document.getElementById("pwd").value=""
       }
     
 }
